846b1ee7e3a5df10b61df2fbbd529ff39b2dfd4a
zhangs01 <czs208112@gmail.com>
2018-08-21 16:50:39 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="hello">
+    <el-container>
+      <el-aside v-bind:style="{height:asideHeight,width:'230px'}">
+        <el-row class="tac">
+          <el-col :span="24">
+            <h5 style="text-align: center;background-color:#303F50;margin:0px;padding:15px 0px">张三的站</h5>
+            <el-menu
+              default-active="2"
+              class="el-menu-vertical-demo"
+              @open="handleOpen"
+              @close="handleClose"
+              background-color="#303F50"
+              text-color="#fff"
+              active-text-color="#ffd04b">
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航一</span>
+                </template>
+                <el-menu-item-group>
+                  <template slot="title">分组一</template>
+                  <el-menu-item index="1-1">选项1</el-menu-item>
+                  <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                  <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                  <template slot="title">选项4</template>
+                  <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+              </el-submenu>
+              <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+              </el-menu-item>
+              <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+
+      </el-aside>
+      <el-container>
+        <el-header>
+          <el-breadcrumb separator=">">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+          </el-breadcrumb>
+        </el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      asideHeight: window.innerHeight + 'px',
+      msg: 'Welcome to Your Vue.js App',
+      show: true
+    }
+  },
+  mounted () {
+  },
+  methods: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .el-menu {
+    border-right: solid 0px
+  }
+
+  .el-header, .el-footer {
+    background-color: cadetblue;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #303F50;
+    color: white;
+    text-align: left;
+    line-height: 25px;
+  }
+
+  .el-breadcrumb {
+    margin-top: 20px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 400px;
+  }
+
+  body > .el-container {
+    margin-bottom: 10px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 220px;
+  }
+</style>
